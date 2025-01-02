2a7c24c4de42f0b13a726e344035b51dcc5823c8
Gabriel Pires <gp0259934@gmail.com>
2025-01-02 20:42:50 -0300

    feat:  Melhorias no menu, modularização e ajustes nas mensagens

--- Organizador_pessoal/src/main/java/com/ead/Main.java
+++ Organizador_pessoal/src/main/java/com/ead/Main.java
@@ -8,161 +8,183 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
+
+
 public class Main
 {
+    static List<CadastrarAtividade> cadastrarList = new ArrayList<>();
+    static SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yy");
+    static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException
+    {
+        int menu;
+        do
+        {
+            System.out.println("Menu:");
+            System.out.println("1 - Adicionar Atividade");
+            System.out.println("2 - Listar Atividades");
+            System.out.println("3 - Editar Atividade");
+            System.out.println("4 - Excluir Atividade");
+            System.out.println("0 - Finalizar Programa");
 
-        Scanner sc = new Scanner(System.in);
-        List<CadastrarAtividade> cadastrarList = new ArrayList<>();
-        SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yy");
+           menu = sc.nextInt();
 
-        int id =1;
-        int menu =1;
+            switch (menu)
+            {
 
-        while(menu != 0){
-            System.out.println("Menu\n" +
-                    "1 - Adicionar Atividade\n" +
-                    "2 - Listar\n" +
-                    "3 - Editar\n" +
-                    "4 - Excluir\n" +
-                    "0 - Finalizar programa");
-            //MENU
-            menu = sc.nextInt(); //ok
-
-            switch (menu) {
-
-                //Cadastro atividades
                 case 1:
-                    System.out.println("Quantas Atividades irão ser registradas: ");
-                    int quantidadeAtividade = sc.nextInt();
-
-                    sc.nextLine(); // Consumir a linha restante no buffer
-
-                    for (int i = 1; i <= quantidadeAtividade; i++)
-                    {
-
-                        System.out.println("Digite o nome da atividade: ");
-                        String nome = sc.nextLine();
-
-                        System.out.println("Descrição: ");
-                        String descricao = sc.nextLine();
-
-                        System.out.println("Qual dia vai ocorrer a atividade (dd/MM/yy): ");
-                        String data = sc.nextLine();
-
-                        Date  date = dataFormatada.parse(data);
-                        CadastrarAtividade cadastrarAtividade = new CadastrarAtividade(nome, descricao,date,id);
-                        cadastrarList.add(cadastrarAtividade);
-                        id++;
-                    }
-                    break;//ok
+                    CadastrarAtividade();
+                    break;
 
                 case 2:
-                    if(cadastrarList.size() != 0)
+                    if (cadastrarList.size() != 0)
                     {
-                        System.out.println("Lista de Atividades: \n");
-                        for (CadastrarAtividade i : cadastrarList)
-                        {
-                            System.out.println("Nome: " + i.getNomeAtividade());
-                            System.out.println("Data:" + dataFormatada.format(i.getData()));
-                            System.out.println("Descrição: " + i.getDescricao());
-                        }
+                        ListarAtividades();
                     }
                     else
                     {
-                        System.out.println("Adicione uma tarefa para ser listada");
+                        System.out.println("Nenhuma atividade cadastrada. Adicione uma atividade para ser listada.");
                     }
 
-                    break; //ok
+                    break;
 
                 case 3:
 
                     if (cadastrarList.size() != 0)
                     {
-                        System.out.println("Atividades disponíveis:");
-                        for (CadastrarAtividade i : cadastrarList)
-                        {
-                            System.out.println("id: " + i.getId());
-                            System.out.println("Nome: " + i.getNomeAtividade());
-                            System.out.println("Data:" + dataFormatada.format(i.getData()));
-                            System.out.println("Descrição: " + i.getDescricao());
-                        }
-
-                        System.out.println("Digite o ID da atividade que deseja Editar:");
-                        int buscaIdEditar = sc.nextInt();
-                        sc.nextLine(); // Consumir a linha restante no buffer
-
-                        // Buscar atividade pelo ID
-                        for (CadastrarAtividade i : cadastrarList)
-                        {
-                            if (i.getId() == buscaIdEditar)
-                            {
-                                System.out.println("Nome: ");
-                                String nome = sc.nextLine();
-                                i.setNomeAtividade(nome);
-                                System.out.println("Descrição: ");
-                                String descricao = sc.nextLine();
-                                i.setDescricao(descricao);
-                                System.out.println("Data nova atividade (dd/MM/yy): ");
-                                String data = sc.nextLine();
-                                Date  date = dataFormatada.parse(data);
-                                i.setData(date);
-                                System.out.println("Atividade editada com sucesso!");
-                            }
-
-                        }
-
-                    } else
-                    {
-                        System.out.println("Não há atividades para excluir.");
-                    }
-                    break;
-
-                case 4:
-
-                    if (cadastrarList.size() != 0) {
-                        System.out.println("Atividades disponíveis:");
-                        for (CadastrarAtividade i : cadastrarList) {
-                            System.out.printf("ID: %d | Nome: %s%n", i.getId(), i.getNomeAtividade());
-                        }
-
-                        System.out.println("Digite o ID da atividade que deseja excluir:");
-                        int resp = sc.nextInt();
-
-                        // Buscar atividade pelo ID
-                        CadastrarAtividade atividadeToRemove = null;
-                        for (CadastrarAtividade i : cadastrarList) {
-                            if (i.getId() == resp) {
-                                atividadeToRemove = i;
-                                break;
-                            }
-                        }
-
-                        if (atividadeToRemove != null) {
-                            cadastrarList.remove(atividadeToRemove);
-                            System.out.println("Atividade removida com sucesso!");
-                        } else {
-                            System.out.println("ID não encontrado. Nenhuma atividade foi removida.");
-                        }
+                        ExcluirAtividade();
                     }
                     else
                     {
                         System.out.println("Não há atividades para serem editadas.");
                     }
                     break;
+
+                case 4:
+
+                    if (cadastrarList.size() != 0)
+                    {
+                        EditarAtividade();
+                    }
+                    else
+                    {
+                        System.out.println("Não há atividades para excluir.");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Finalizando programa");
+                    break;
+
                 default:
-                    System.out.println("Digite novamente!");
+                    System.out.println("Opção inválida. Por favor, digite uma opção válida.");
             }
-        };
+        }while(menu != 0);
     }
 
 
-   /*Futuramente adicionar modularização
+   /*Futuramente adicionar modularização*/
 
-     public void Cadastrar()
+    public static void ListarAtividades()
     {
-    }*/
+
+            System.out.println("\n===== Lista de Atividades =====");
+            for (CadastrarAtividade i : cadastrarList)
+            {
+                    System.out.println("ID: " + i.getId());
+                    System.out.println("Nome: " + i.getNomeAtividade());
+                    System.out.println("Data: " + dataFormatada.format(i.getData()));
+                    System.out.println("Descrição: " + i.getDescricao());
+                    System.out.println("-------------------------------");
+            }
+    }
+
+    public static void EditarAtividade() throws ParseException
+    {
+
+        ListarAtividades();
+
+        System.out.println("Digite o ID da atividade que deseja editar: ");
+        int buscaIdEditar = sc.nextInt();
+        sc.nextLine(); // Consumir a linha restante no buffer
+
+        // Buscar atividade pelo ID
+        for (CadastrarAtividade i : cadastrarList)
+        {
+            if (i.getId() == buscaIdEditar)
+            {
+                System.out.println("Novo nome da atividade: ");
+                String nome = sc.nextLine();
+                i.setNomeAtividade(nome);
+                System.out.println("Nova descrição da atividade: ");
+                String descricao = sc.nextLine();
+                i.setDescricao(descricao);
+                System.out.println("Nova data da atividade (dd/MM/yy): ");
+                String data = sc.nextLine();
+                Date  date = dataFormatada.parse(data);
+                i.setData(date);
+                System.out.println("Atividade editada com sucesso!");
+            }
+
+        }
+
+    }
+
+    public static void ExcluirAtividade()
+    {
+        ListarAtividades();
+
+        System.out.println("Digite o ID da atividade que deseja excluir: ");
+        int resp = sc.nextInt();
+
+        // Buscar atividade pelo ID
+        CadastrarAtividade atividadeToRemove = null;
+        for (CadastrarAtividade i : cadastrarList) {
+            if (i.getId() == resp) {
+                atividadeToRemove = i;
+                break;
+            }
+        }
+
+        if (atividadeToRemove != null)
+        {
+            cadastrarList.remove(atividadeToRemove);
+            System.out.println("Atividade removida com sucesso!");
+        }
+        else
+        {
+            System.out.println("ID não encontrado. Nenhuma atividade foi removida.");
+        }
+    }
+
+    public static void CadastrarAtividade() throws ParseException
+    {
+        System.out.println("Quantas atividades você deseja registrar? ");
+        int quantidadeAtividade = sc.nextInt();
+
+        sc.nextLine(); // Consumir a linha restante no buffer
+
+        for (int i = 1; i <= quantidadeAtividade; i++)
+        {
+
+            System.out.println("\n----- Atividade " + i + " -----");
+
+            System.out.print("Nome da atividade: ");
+            String nome = sc.nextLine();
+
+            System.out.print("Descrição da atividade: ");
+            String descricao = sc.nextLine();
+
+            System.out.print("Data da atividade (dd/MM/yy): ");
+            String data = sc.nextLine();
+
+            Date  date = dataFormatada.parse(data);
+            int id = cadastrarList.size() + 1;
+            CadastrarAtividade cadastrarAtividade = new CadastrarAtividade(nome, descricao,date,id);
+            cadastrarList.add(cadastrarAtividade);
+        }
+    }
 
 }
 
